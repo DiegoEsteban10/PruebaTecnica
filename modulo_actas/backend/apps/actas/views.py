415a700766df934ffbe574eb034dc33930485249
DiegoEsteban10 <egoessanrero@gmail.com>
2025-08-10 19:10:45 -0500

    Correcciones de errores en el backend

--- modulo_actas/backend/apps/actas/views.py
+++ modulo_actas/backend/apps/actas/views.py
@@ -1,12 +1,10 @@
 from rest_framework import viewsets, permissions
 from apps.actas.models import Acta
-from apps.compromisos.models import Compromiso
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from .serializers import ActaSerializer
 from rest_framework.decorators import action
 from apps.actas.serializers import ActaSerializer
-from apps.compromisos.serializers import CompromisoSerializer
 from rest_framework.views import APIView
 from django.http import Http404, FileResponse
 # Create your views here.
@@ -23,16 +21,16 @@ class ActaViewSet(viewsets.ModelViewSet):
             return Acta.objects.all()
         return Acta.objects.filter(creador=usuario)
     
-    @action(detail=False, methods=['get'])
-    def pdf(sefl,request, pk=None):
-        acta =  sefl.get_object()
+    @action(detail=True, methods=['get'])
+    def pdf(self,request, pk=None):
+        acta =  self.get_object()
         return FileResponse(open(acta.archivo_pdf.path, 'rb'))
                             
     def perform_create(self, serializer):
         serializer.save(creador=self.request.user)
         
 class ActaFileview(APIView):
-    permissions_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     
     def get(self, request, pk):
         try:
@@ -41,16 +39,3 @@ class ActaFileview(APIView):
             return FileResponse(open(file_path, 'rb'), content_type='application/pdf')
         except (Acta.DoesNotExist, FileNotFoundError):
             raise Http404
-        
-class CompromisoViewSet(viewsets.ModelViewSet):
-    serializer_class = CompromisoSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    
-    def get_queryset(self):
-        return Compromiso.objects.filter(acta__creador=self.request.user)
-    
-    def perform_create(self, serializer):
-        acta = serializer.validated_data['acta']
-        if acta.creador != self.request.user:
-            raise PermissionDenied("No tienes permiso para crear compromisos en esta acta.")
-        serializer.save()   
--- modulo_actas/backend/apps/compromisos/models.py
+++ modulo_actas/backend/apps/compromisos/models.py
@@ -3,6 +3,12 @@ from apps.actas.models import Acta
 
 # Create your models here.
 class Compromiso(models.Model):
+    ESTADOS = [
+    ('PENDIENTE', 'Pendiente'),
+    ('COMPLETADO', 'Completado'),       
+    ]
+    
+    estado = models.CharField(max_length=15, choices=ESTADOS, default='PENDIENTE')
     descripcion = models.TextField('Descripción')
     fecha_limite = models.DateField('Fecha límite')
     acta = models.ForeignKey('actas.Acta', on_delete=models.CASCADE, related_name='compromisos')
